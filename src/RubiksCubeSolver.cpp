b173765ea53074c63bac9358cfc1c0a65c5cd1fa
Tan Grumser <tan.grumser@gmail.com>
2022-02-28 17:00:31 +0100

    initial commit

---
+++ src/RubiksCubeSolver.cpp
@@ -0,0 +1,17 @@
+// Your First C++ Program
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int test;
+    std::cout << "Hello world!" << '\n';
+    std::cin >> test;
+    std::cout << test << '\n';
+    
+    std::cout << "Any last words?...\n";
+    std::cin >> test;
+    return 0;
+}
