23f6ac1b6b6a50b4dd3e0ea54fb150132f65ca9a
Tan Grumser <tan.grumser@gmail.com>
2022-03-21 14:57:54 +0100

    Implemented a non memory allocating IDA* version.

--- src/IterativeDeepeningAStar.cpp
+++ src/IterativeDeepeningAStar.cpp
@@ -5,18 +5,21 @@
 #include "Turn.h"
 #include <algorithm>
 
-int Search(vector<RubicsCubeState*> *path, int depth, int bound, Turn lastTurn);
+int Search(RubicsCubeState* state, vector<Turn>* path, int depth, int bound, Turn lastTurn);
 vector<Turn> GenerateTurnSequenceFromStateSequence(vector<RubicsCubeState*> stateSequence);
 const int MAX_BOUND = 100000;
 int searchedStates = 0;
 
 vector<Turn> Solver::IterativeDeepeningAStar(RubicsCubeState* startState) {
         int bound = Solver::GetDistanceHeuristic(startState, RubicsCubeState::InitialState());
-        vector<RubicsCubeState*> path = {};
-        path.push_back(startState);
+        vector<Turn>* path = new std::vector<Turn>(20, Turn::Empty());
+
+        for (int i = 0; i < path->size(); i++) {
+            (*path)[i] = Turn::Empty();
+        }
 
         while (true) {
-            int newBound = Search(&path, 0, bound, Turn::Empty());
+            int newBound = Search(startState, path, 0, bound, Turn::Empty());
 
             if (newBound == -1) {
                 break;
@@ -29,18 +32,21 @@ vector<Turn> Solver::IterativeDeepeningAStar(RubicsCubeState* startState) {
             bound = newBound;
         }
 
-        return GenerateTurnSequenceFromStateSequence(path);
+        while (path->back().Equals(Turn::Empty())) {
+            path->pop_back();
+        }
+
+        return *path;
     }
 
-int Search(vector<RubicsCubeState*> *path, int depth, int bound, Turn lastTurn) {
-    RubicsCubeState* node = path->back();
-    int totalEstimatedCost = depth + Solver::GetDistanceHeuristic(node, RubicsCubeState::InitialState());
+int Search(RubicsCubeState* state, vector<Turn>* path, int depth, int bound, Turn lastTurn) {
+    int totalEstimatedCost = depth + Solver::GetDistanceHeuristic(state, RubicsCubeState::InitialState());
     
     if (totalEstimatedCost > bound) {
         return totalEstimatedCost;
     } 
 
-    if (node->Equals(RubicsCubeState::InitialState())) {
+    if (state->Equals(RubicsCubeState::InitialState())) {
         return -1;
     } 
 
@@ -48,29 +54,24 @@ int Search(vector<RubicsCubeState*> *path, int depth, int bound, Turn lastTurn) 
 
     // TODO allocating over and over again isn't necessary. Rather a stack with pre allocated states should be used.
 
-    RubicsCubeState* succesor = node->Copy();
-    path->push_back(succesor);
-
     for (int i = 0; i < Turn::CountAllTurns; i++) {
         if (!Turn::AllTurns[i].IsTurnBacktracking(lastTurn)) {
-            succesor->ApplyTurn(Turn::AllTurns[i]);
+            (*path)[depth] = Turn::AllTurns[i];
+            state->ApplyTurn(Turn::AllTurns[i]);
             
-            int newBound = Search(path, depth + 1, bound, Turn::AllTurns[i]);
+            int newBound = Search(state, path, depth + 1, bound, Turn::AllTurns[i]);
 
             if (newBound == -1) {
                 return -1;
-            }
+            }            
 
             if (newBound < min) {
                 min = newBound;
             } 
 
-            succesor->ApplyTurn(Turn::AllTurns[i].Inverse());
+            state->ApplyTurn(Turn::AllTurns[i].Inverse());
         }
     }
-
-    delete path->back();
-    path->pop_back();
 
     return min;
 }
