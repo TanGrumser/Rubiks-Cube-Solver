b449c76edc204c5e940241946c996942cd3aa97e
Tan Grumser <tan.grumser@gmail.com>
2022-06-15 17:04:30 +0200

    state index hashing improvments

--- src/StateIndexHasher.h
+++ src/StateIndexHasher.h
@@ -1,11 +1,23 @@
 #include <iterator>
 #include "StateIndex.h"
+#include "robin-map/robin_hash.h"
+#include <functional>
+
+template <class T>
+inline void hash_combine(std::size_t & s, const T & v) {    
+    s ^= std::hash<T>{}(v) + 0x9e3779b9 + (s<< 6) + (s>> 2);
+}
 
 struct StateIndexHasher {
   std::size_t operator()(const StateIndex& k) const {
     using std::size_t;
     using std::hash;
+    size_t h = 17;
 
+    hash_combine(h, k.edgeIndex);
+    hash_combine(h, k.cornerIndex);
+
+    return h;
     return (k.cornerIndex * k.edgeIndex) ^ k.cornerIndex  ^ k.edgeIndex; 
   }
 };
